46a03b142bf869d58b6ec2b4f77318ef2b8e066f
caiyang <72679095+caiyang2020@users.noreply.github.com>
2021-04-14 00:23:56 +0800

    增加数字翻转介绍

--- 数字反转.py
+++ 数字反转.py
@@ -1,12 +1,22 @@
-def numFlipOver(num:int) ->int:
+"""
+    num为一个int类型数字
+    本方法可以将其翻转
+    如：
+        -123 -> -321
+        123  ->  321
+        120  ->  21
+"""
+
+
+def numFlipOver(num: int) -> int:
     num = str(num)
-    if num[0]=="-":
-        num =int(num[:0:-1])
+    if num[0] == "-":
+        num = int(num[:0:-1])
         print(-num)
     else:
         num = int(num[::-1])
         print(num)
-        
+
 
 if __name__ == "__main__":
     numFlipOver(-123)
