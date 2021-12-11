28c086be9862bab8546e4d2d60185b2b4e50d3b6
caiyang <72679095+caiyang2020@users.noreply.github.com>
2021-12-11 12:23:43 +0800

    Update 两数之和.py

--- 两数之和.py
+++ 两数之和.py
@@ -5,17 +5,18 @@
     返回两个数的位置
 """
 
+
 class Solution:
-    def twoSum(self,nums:[int],target:int):
-        n = len(nums) 
+    def twoSum(self, nums: int([]), target: int):
+        n = len(nums)
         for i in range(n):
-            for j in range(i+1,n):
+            for j in range(i+1, n):
                 if nums[i] + nums[j] == target:
-                    return [i,j]
+                    return [i, j]
+
 
 if __name__ == "__main__":
     test1 = Solution()
-    sum1=test1.twoSum([1,2,3,4,5,6,7,8,9],10)
+    sum1 = test1.twoSum([1, 2, 3, 4, 5, 6, 7, 8, 9], 10)
     # test1 = Solution().twoSum(123123,123123)
     print(sum1)
-
